4e4172494630a7d2ee636b50441e05c23377b3be
StefansArya <stefansarya1@gmail.com>
2022-02-21 17:55:56 +0700

    Sync node movement

--- js/src/RemoteSketch.js
+++ js/src/RemoteSketch.js
@@ -44,6 +44,7 @@ class RemoteSketch extends RemoteEngineClient {
 		let { ifaceList } = instance;
 
 		let that = this;
+		let $window = $(window);
 		let uid = Math.random()*10000 | 0; // ToDo: replace this on the relay server
 
 		function keyup(ev){
@@ -62,27 +63,45 @@ class RemoteSketch extends RemoteEngineClient {
 
 		let container = this.instance.scope('container');
 		function pointerdown(ev){
-			if(that._skipEvent || !ev.isTrusted) return;
+			if(that._skipEvent || !ev.isTrusted || ev.button !== 0) return;
 			let node = ev.target.closest('.nodes .node');
 			let cable = ev.target.closest('.cables g');
 
 			if(node != null){
+				let iface = node.model;
 				let i = ifaceList.indexOf(node.model);
-				that._onSyncOut({uid, t:'skc', w:'npd', x:ev.clientX-container.pos.x, y:ev.clientY-container.pos.y, i});
+
+				that._onSyncOut({uid, w:'skc', t:'npd', x:ev.clientX-container.pos.x, y:ev.clientY-container.pos.y, i});
+
+				$window.once('pointerup', () => {
+					that._onSyncOut({uid, w:'skc', t:'npu', x:iface.x, y:iface.y, i});
+				}, {capture: true});
 			}
 			else if(cable != null){
 				cable = cable.model;
 				let ci = container.cableScope.list.indexOf(cable);
 
-				that._onSyncOut({uid, t:'skc', w:'cpd',
+				that._onSyncOut({uid, w:'skc', t:'cpd',
 				    x:ev.clientX-container.pos.x,
 				    y:ev.clientY-container.pos.y,
 					ci,
 				});
+
+				$window.once('pointerup', () => {
+					that._onSyncOut({uid, w:'skc', t:'cpu',
+					    x:cable.head2[0],
+					    y:cable.head2[1],
+						ci,
+					});
+				}, {capture: true});
 			}
 			else {
-				if(instance.scope('container').select.show){ // selecting
-					that._onSyncOut({uid, t:'skc', w:'selpd', x:ev.clientX-container.pos.x, y:ev.clientY-container.pos.y});
+				if(container.select.show){ // selecting
+					that._onSyncOut({uid, w:'skc', t:'selpd', x:ev.clientX-container.pos.x, y:ev.clientY-container.pos.y});
+
+					$window.once('pointerup', () => {
+						that._onSyncOut({uid, w:'skc', t:'selpu', x:ev.clientX-container.pos.x, y:ev.clientY-container.pos.y});
+					}, {capture: true});
 				}
 			}
 		}
@@ -109,24 +128,66 @@ class RemoteSketch extends RemoteEngineClient {
 			that._onSyncOut({uid, w:'skc', t:'pm', x:ev.clientX-container.pos.x, y:ev.clientY-container.pos.y});
 		}
 
-		$(window)
-			.on('keyup', keyup)
-			.on('pointerdown', pointerdown)
-			.on('pointermove', pointermove);
+		$window
+			.on('keyup', keyup, {capture: true})
+			.on('pointerdown', pointerdown, {capture: true})
+			.on('pointermove', pointermove, {capture: true});
+
+		instance.on('cable.created', cableCreated);
+		function cableCreated({ port, cable }){
+			that._onSyncOut({uid, w:'skc', t:'ccd'});
+
+			$window.once('pointerup', () => {
+				if(cable._evDisconnected) return;
+
+				let list = container.cableScope.list;
+				let ci = list.indexOf(cable);
+
+				that._onSyncOut({uid, w:'skc', t:'ccu', x:ev.clientX-container.pos.x, y:ev.clientY-container.pos.y, ci});
+			}, {capture: true});
+		}
+
+		instance.on('cable.created.branch', cableCreatedBranch);
+		function cableCreatedBranch({ cable }){
+			let list = container.cableScope.list;
+
+			let pci = list.indexOf(cable.parentCable);
+			that._onSyncOut({uid, w:'skc', t:'ccbd', pci});
+
+			$window.once('pointerup', () => {
+				if(cable._evDisconnected) return;
+				let ci = list.indexOf(cable);
+
+				that._onSyncOut({uid, w:'skc', t:'ccbu', x:ev.clientX-container.pos.x, y:ev.clientY-container.pos.y, pci, ci});
+			}, {capture: true});
+		}
+
+		instance.on('cable.deleted', cableDeleted);
+		function cableDeleted({ cable }){
+			let list = container.cableScope.list;
+			let ci = list.indexOf(cable);
+
+			cable._evDisconnected = true;
+			that._onSyncOut({uid, w:'skc', t:'cd', ci});
+		}
 
 		this.destroy = function(){
-			$(window)
-				.off('keyup', keyup)
-				.off('pointerdown', pointerdown)
-				.off('pointermove', pointermove);
+			$window
+				.off('keyup', keyup, {capture: true})
+				.off('pointerdown', pointerdown, {capture: true})
+				.off('pointermove', pointermove, {capture: true});
+
+			instance.off('cable.created', cableCreated);
+			instance.off('cable.deleted', cableDeleted);
 		}
 	}
 
 	onSyncIn(data){
 		data = super.onSyncIn(data);
 		if(data == null) return;
+		if(window.aaa) console.log(data);
 
-		if(data.t === 'kd'){
+		if(data.w === 'kd'){ // keydown
 			let { ifaceList } = this.instance;
 			let iface = ifaceList[data.i];
 			let el = elementChildIndexes(data.s, iface.$el[0].children[0]);
@@ -136,41 +197,108 @@ class RemoteSketch extends RemoteEngineClient {
 			return;
 		}
 
-		if(data.t === 'skc'){
-			if(data.w === 'npd'){
-				// c
+		if(data.w === 'skc'){ // sketch event
+			if(data.t === 'selpd'){ // selection pointer down
+				// ToDo
 			}
-			else if(data.w === 'cpd'){
-				// c
+			else if(data.t === 'selpu'){ // selection pointer up
+				// ToDo
 			}
-			else if(data.w === 'selpd'){
-				// c
+			else if(data.t.slice(0, 1) === 'n'){ // node
+				let container = this.instance.scope('container');
+				let { ifaceList } = this.instance;
+				let iface;
+
+				if(data.i != null){
+					iface = ifaceList[data.i];
+					if(iface == null) throw new Error("Node list was not synced");
+				}
+
+				if(data.t === 'npd'){ // node pointer down
+					// ToDo
+				}
+				else if(data.t === 'npu'){ // node pointer up
+					iface.x = data.x;
+					iface.y = data.y;
+				}
 			}
-			return;
+			else if(data.t.slice(0, 1) === 'c'){ // cable
+				let container = this.instance.scope('container');
+				let cables = container.cableScope;
+				let cable, parentCable;
+
+				if(data.ci != null){
+					cable = cables.list[data.ci];
+					if(cable == null) throw new Error("Cable list was not synced");
+				}
+
+				if(data.pci != null){
+					parentCable = cables.list[data.pci];
+					if(parentCable == null) throw new Error("Cable list was not synced");
+				}
+
+				if(data.t === 'cpd'){ // cable pointer down
+					// ToDo
+				}
+				else if(data.t === 'cpu'){ // cable pointer up
+					cable.head2[0] = data.x;
+					cable.head2[1] = data.y;
+				}
+
+				else if(data.t === 'ccd'){ // cable created down
+					// ToDo
+				}
+				else if(data.t === 'ccu'){ // cable created up
+					cable.head2[0] = data.x;
+					cable.head2[1] = data.y;
+				}
+
+				else if(data.t === 'ccbd'){ // cable created branch down
+					this._skipEvent = true;
+					cable.createBranch();
+					this._skipEvent = false;
+				}
+				else if(data.t === 'ccbu'){ // cable created branch up
+					cable.head2[0] = data.x;
+					cable.head2[1] = data.y;
+				}
+
+				else if(data.t === 'cd'){ // cable deleted
+					this._skipEvent = true;
+					cable.disconnect();
+					this._skipEvent = false;
+				}
+			}
+			else if(data.t.slice(0, 1) === 'p'){ // pointer
+				let { remotes } = this._scope;
+				let cursor;
+
+				for (var i = 0; i < remotes.length; i++) {
+					if(remotes[i].uid === data.uid){
+						cursor = remotes[i];
+						break
+					}
+				}
+
+				if(cursor == null){
+					cursor = { uid: data.uid };
+					remotes.push(cursor);
+				}
+
+				let container = this.instance.scope('container');
+
+				cursor.x = data.x - container.offset.x;
+				cursor.y = data.y - container.offset.y;
+
+				if(data.t === 'pd'){ // pointer down
+					// ToDo
+				}
+				else if(data.t === 'pm'){ // pointer move
+					// ToDo
+				}
+			}
+			else throw new Error("Unhandled sketch control: "+ data.t);
 		}
-
-		let { remotes } = this._scope;
-		let cursor;
-
-		for (var i = 0; i < remotes.length; i++) {
-			if(remotes[i].uid === data.uid){
-				cursor = remotes[i];
-				break
-			}
-		}
-
-		if(cursor == null){
-			cursor = { uid: data.uid };
-			remotes.push(cursor);
-		}
-
-		let container = this.instance.scope('container');
-
-		cursor.x = data.x - container.offset.x;
-		cursor.y = data.y - container.offset.y;
-
-		if(data.t === 'pd'){}
-		else if(data.t === 'pm'){}
 	}
 }
 
@@ -197,6 +325,7 @@ win.onclick = function(){
 
 	let ins = new Blackprint.RemoteSketch(SketchList[0]);
 	window.onmessage = function(msg){ ins.onSyncIn(msg.data) };
+	window.onbeforeunload = ()=> win.close();
 	ins.onSyncOut = v => win.postMessage(v);
 }
  */
