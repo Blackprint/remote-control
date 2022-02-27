016532a8ff464f1cd2183bd9808ff40a946cd22b
StefansArya <stefansarya1@gmail.com>
2022-02-27 12:19:09 +0700

    Split to multiple file

---
+++ js/src/RemoteBase.js
@@ -0,0 +1,68 @@
+// ToDo:
+// - sync data (_node.sync)
+
+class RemoteBase extends Blackprint.Engine.CustomEvent {
+	// true  => allow
+	// false => block
+	async onImport(json){return false}
+	async onModule(urls){return false}
+
+	// "onSyncOut" function need to be replaced and the data need to be send to remote client
+	onSyncOut(data){}
+	_onSyncOut(data){ this.onSyncOut(JSON.stringify(data)) }
+
+	constructor(instance){
+		super();
+		this.instance = instance;
+		this._skipEvent = false;
+	}
+
+	async _syncModuleList(urls){
+		this._skipEvent = true;
+
+		if(await this.onModule(urls) === true){
+			// Import from editor
+			this._skipEvent = true;
+
+			let oldList = Object.keys(Blackprint.modulesURL);
+			let removed = [];
+
+			for (var i = oldList.length - 1; i >= 0; i--) {
+				var url = oldList[i];
+				let index = urls.indexOf(url);
+
+				// Remove module
+				if(index === -1){
+					Blackprint.deleteModuleFromURL(url);
+					removed.push(url);
+					continue;
+				}
+
+				urls.splice(index, 1);
+			}
+
+			if(removed.length !== 0)
+				this.emit('module.remove', {list: removed});
+
+			if(urls.length !== 0){
+				console.log(`Adding ${urls.length} new module triggered by remote sync`);
+				this.emit('module.add', {list: urls});
+				Blackprint.loadModuleURL(urls, {
+					loadBrowserInterface: Blackprint.Sketch != null
+				});
+			}
+
+			this._skipEvent = true;
+		}
+		else {
+			// Disable remote on blocked module sync
+			console.error("Loaded module sync was denied, the remote control will be disabled");
+			this.onSyncIn = ()=>{};
+			this.onSyncOut = ()=>{};
+			this.emit('disabled');
+			this._skipEvent = true;
+		}
+
+		this._skipEvent = false;
+	}
+}
---
+++ js/src/RemoteEngine.js
@@ -0,0 +1,155 @@
+// ToDo: port to PHP, Golang, and other programming languages
+class RemoteEngine extends RemoteBase {
+	constructor(instance){
+		super(instance);
+
+		let { ifaceList } = instance;
+		Blackprint.settings('_remoteEngine', true);
+
+		let evCableDisconnect;
+		instance.on('cable.disconnect', evCableDisconnect = ({ cable }) => {
+			if(cable._evDisconnected || this._skipEvent) return;
+			this._onSyncOut({
+				w:'c',
+				inp:{i: ifaceList.indexOf(cable.input.iface), s: cable.input.source, n: cable.input.name},
+				out:{i: ifaceList.indexOf(cable.output.iface), s: cable.output.source, n: cable.output.name},
+				t:'d'
+			});
+		});
+
+		let evFlowEvent;
+		instance.on('_flowEvent', evFlowEvent = cable => {
+			if(this._skipEvent) return;
+			this._onSyncOut({
+				w:'c',
+				inp:{i: ifaceList.indexOf(cable.input.iface), s: cable.input.source, n: cable.input.name},
+				out:{i: ifaceList.indexOf(cable.output.iface), s: cable.output.source, n: cable.output.name},
+				t:'f'
+			});
+		});
+
+		let evNodeSync;
+		instance.on('_node.sync', evNodeSync = ev => {
+			if(this._skipEvent) return;
+			this._onSyncOut({w:'nd', i:ifaceList.indexOf(ev.iface), d: ev.data, t:'s'})
+		});
+
+		let evError;
+		instance.on('error', evError = ev => {
+			if(this._skipEvent) return;
+			this._onSyncOut({w:'err', d: ev.data})
+		});
+
+		// instance.on('cable.connecting', cable => {});
+		// instance.on('cable.cancel', cable => {});
+		// instance.on('port.output.call', cable => {});
+		// instance.on('port.output.value', cable => {});
+
+		this.destroy = () => {
+			instance.off('cable.disconnect', evCableDisconnect);
+			instance.off('_flowEvent', evFlowEvent);
+			instance.off('_node.sync', evNodeSync);
+			instance.off('error', evError);
+
+			this.onSyncIn = ()=>{};
+			this.onSyncOut = ()=>{};
+		}
+	}
+
+	async onSyncIn(data){
+		data = JSON.parse(data);
+		let { ifaceList } = this.instance;
+
+		if(data.w === 'c'){ // cable
+			let {inp, out} = data;
+			let ifaceInput = ifaceList[inp.i];
+			let ifaceOutput = ifaceList[out.i];
+
+			if(data.t === 'c'){ // connect
+				let inputPort = ifaceInput[inp.s][inp.n];
+				let outputPort = ifaceOutput[out.s][out.n];
+
+				inputPort.connectPort(outputPort);
+				return;
+			}
+
+			let cables = ifaceInput[inp.s][inp.n].cables;
+			let cable;
+			for (var i = 0; i < cables.length; i++) {
+				if(cables[i].output === ifaceOutput){
+					cable = cables[i];
+					break;
+				}
+			}
+
+			if(cable == null) return;
+
+			if(data.t === 'd'){ // disconnect
+				cable._evDisconnected = true;
+				cable.disconnect();
+			}
+		}
+		else if(data.w === 'nd'){ // node
+			let iface = ifaceList[data.i];
+
+			if(data.t === 's'){ // sync
+				if(iface != null)
+					throw new Error("Node list was not synced");
+
+				iface.node.syncIn?.(data.d);
+			}
+			else if(data.t === 'c'){ // created
+				if(iface != null) throw new Error("Node list was not synced");
+				let newIface = this.instance.createNode(data.nm);
+
+				if(ifaceList.indexOf(newIface) !== data.i)
+					throw new Error("Node list was not synced");
+			}
+			else if(data.t === 'd'){ // deleted
+				if(iface == null) throw new Error("Node list was not synced");
+				this.instance.deleteNode(iface);
+			}
+		}
+		else if(data.w === 'ins'){ // instance
+			let instance = this.instance;
+
+			if(data.t === 'c'){ // clean nodes
+				this._skipEvent = true;
+				instance.clearNodes();
+				this._skipEvent = false;
+			}
+			else if(data.t === 'ci'){ // clean import
+				this._skipEvent = true;
+				// instance.clearNodes();
+
+				if(await this.onImport() === true){
+					this._skipEvent = true;
+					this.emit('sketch.import', {data: data.d});
+					await instance.importJSON(data.d);
+					this.emit('sketch.imported', {data: data.d});
+					this._skipEvent = false;
+				}
+
+				this._skipEvent = false;
+			}
+			else if(data.t === 'sml') // sync module list
+				this._syncModuleList(data.d);
+			else if(data.t === 'nidc'){ // node id changed
+				this._skipEvent = true;
+
+				let old = instance.iface[data.from];
+				if(old == null)
+					throw new Error("Node list was not synced");
+
+				// This may need to be changed if the ID was being used for reactivity
+				delete instance.iface[data.from];
+				instance.iface[data.to] = old;
+				old.id = data.to;
+
+				this._skipEvent = false;
+			}
+		}
+	}
+}
+
+Blackprint.RemoteEngine = RemoteEngine;
