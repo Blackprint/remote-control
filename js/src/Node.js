0b79682e41c83ea7b04c087614d6c202da11b24d
StefansArya <stefansarya1@gmail.com>
2022-03-04 10:34:24 +0700

    Skip some private field to avoid circular data sync

--- js/src/Node.js
+++ js/src/Node.js
@@ -2,6 +2,14 @@ Blackprint.Node.prototype.syncOut = function(id, data){
 	if(this._instance._remote == null || this._syncronizing || this._instance.syncDataOut === false)
 		return;
 
+	if(id.constructor !== String)
+		throw new Error("syncOut's ID must be a string, but got: "+ id);
+
+	let char = id.slice(0, 1);
+	if(char === '_' || char === '$'){
+		throw new Error("syncOut's ID can't be started with '_' or '$' character as it's assumed as a private field, but got: "+ id);
+	}
+
 	if(this.syncThrottle !== 0){
 		if(this._syncWait != null){
 			this._syncHasWait = true;
@@ -12,12 +20,44 @@ Blackprint.Node.prototype.syncOut = function(id, data){
 		this._syncWait = {};
 		setTimeout(()=> {
 			if(this._syncHasWait)
-				this._instance.emit('_node.sync', {iface: this.iface, data: this._syncWait});
+				this._instance.emit('_node.sync', {
+					iface: this.iface,
+					data: clearPrivateField(this._syncWait)
+				});
 
 			this._syncWait = null;
 			this._syncHasWait = false;
 		}, this.syncThrottle);
 	}
 
-	this._instance.emit('_node.sync', {iface: this.iface, data: {[id]: data}});
+	this._instance.emit('_node.sync', {iface: this.iface, data: clearPrivateField({ [id]: data })});
+}
+
+function clearPrivateField(obj){
+	if(obj.constructor === Array){
+		let temp = obj.slice(0);
+		for (var i = 0; i < temp.length; i++) {
+			let ref = temp[i];
+
+			if(ref.constructor === Array || ref.constructor === Object)
+				temp[i] = clearPrivateField(ref);
+		}
+
+		return temp;
+	}
+
+	let temp = {};
+	for(let key in obj){
+		let char = key.slice(0, 1);
+		if(char === '_' || char === '$')
+			continue;
+
+		let ref = obj[key];
+
+		if(ref.constructor === Array || ref.constructor === Object)
+			temp[key] = clearPrivateField(ref);
+		else temp[key] = ref;
+	}
+
+	return temp;
 }
