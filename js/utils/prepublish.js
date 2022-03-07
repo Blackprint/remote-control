28cdb946882c1a2cfc57af95728547c0da09f8eb
StefansArya <stefansarya1@gmail.com>
2022-03-07 18:14:51 +0700

    Add prepublish script and update configurations

---
+++ js/utils/prepublish.js
@@ -0,0 +1,8 @@
+let fs = require('fs');
+
+// Copy file to dist folder
+var temp = fs.readFileSync('../dist/remote-control.min.js', 'utf8');
+fs.writeFileSync('./dist/remote-control.min.js', temp);
+
+var temp = fs.readFileSync('../dist/remote-control.min.js.map', 'utf8');
+fs.writeFileSync('./dist/remote-control.min.js.map', temp);
