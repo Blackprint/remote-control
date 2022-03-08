c64d972a35efede9984a11f247ae49304b392a3c
StefansArya <stefansarya1@gmail.com>
2022-03-08 10:20:00 +0700

    Fix workflows for releasing modules

--- js/utils/prepublish.js
+++ js/utils/prepublish.js
@@ -1,8 +1,9 @@
 let fs = require('fs');
+fs.mkdirSync('./dist');
 
 // Copy file to dist folder
-var temp = fs.readFileSync('../dist/remote-control.min.js', 'utf8');
+var temp = fs.readFileSync('../../dist/remote-control.min.js', 'utf8');
 fs.writeFileSync('./dist/remote-control.min.js', temp);
 
-var temp = fs.readFileSync('../dist/remote-control.min.js.map', 'utf8');
+var temp = fs.readFileSync('../../dist/remote-control.min.js.map', 'utf8');
 fs.writeFileSync('./dist/remote-control.min.js.map', temp);
