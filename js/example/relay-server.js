1d8f331a86209f8bc81a57ef1e5df5c16151acf6
StefansArya <stefansarya1@gmail.com>
2022-03-10 20:03:44 +0700

    Fix relay server

--- js/example/relay-server.js
+++ js/example/relay-server.js
@@ -1,26 +1,28 @@
 // node ./example/relay-server.js
 
 let port = 2345;
-let httpServer = require('http').createServer();
-let io = require('socket.io')(httpServer);
 let connectedTime = new Map();
+let httpServer = require('http').createServer();
+let io = require('socket.io')(httpServer, {
+  cors: { origin: ["http://localhost:6789", "https://blackprint.github.io"] }
+});
 
 io.on('connection', client => {
-	let userID = Math.random() * 1000 | 0;
+	let userId = Math.random() * 1000 | 0;
 	connectedTime.set(client, Date.now());
 
 	client.on('relay', data => {
-		// Let's change the UserID of this client
+		// Let's change the UserId of this client
 		data = JSON.parse(data);
 		data.uid = userId;
 
-		client.broadcast('relay', data);
+		client.broadcast.emit('relay', JSON.stringify(data));
 	});
 
-	client.broadcast('user-connect', { userID });
+	client.broadcast.emit('user-connect', { userId });
 	client.on('disconnect', () =>{
 		connectedTime.delete(client);
-	    client.broadcast('user-disconnect', { userID });
+	    client.broadcast.emit('user-disconnect', { userId });
 	});
 
 	// client.emit('startup-time', engineStartup);
