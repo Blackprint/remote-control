ddc5a5a5d2412ceb3adacb8cffb9eb74c0d6126c
StefansArya <stefansarya1@gmail.com>
2023-05-16 02:49:22 +0700

    Update to v0.1.10

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
 	name='BlackprintRC',
-	version='0.1.9',
+	version='0.1.10',
 	packages=['BlackprintRC'],
 	# scripts=['bin/script1','bin/script2'],
 	url='https://github.com/Blackprint/remote-control',
