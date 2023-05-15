9cd5bf796e5553ad0083b978e338794c94285a73
StefansArya <stefansarya1@gmail.com>
2023-05-15 16:07:13 +0700

    Fix bug for Python

--- python/BlackprintRC/PuppetNode.py
+++ python/BlackprintRC/PuppetNode.py
@@ -1,19 +1,21 @@
 import Blackprint
 import zlib
 import json
+import types
 
 typeList = []
 typeContext = {'__virtualTypes': {}}
 nativeTypeList = [
 	[str, 'String'],
 	[int, 'Integer'],
-	[float, 'Float'],
+	[float, 'Number'],
 	[complex, 'Complex'],
 	[bool, 'Boolean'],
 	[dict, 'Object'],
 	[list, 'List'],
 	[tuple, 'Tuple'],
 	[range, 'Range'],
+	[types.FunctionType, 'Function'],
 	[set, 'Set'],
 	[Blackprint.Types.Any, 'Any'],
 	[Blackprint.Types.Route, 'Route'],
@@ -35,67 +37,72 @@ def getRegisteredNodes(options={}):
 		types = []
 		for i in range(len(list)):
 			port = list[i]
-			portType = port.portType or port
+			portType = port['type'] if isinstance(port, dict) else port
 			nativeType = getNativeType(portType)
 
 			typeIndex = ''
 			if(nativeType == None):
-				typeIndex = typeList.index(portType)
+				typeIndex = typeList.index(portType) if typeList in typeList else -1
 				if(typeIndex == -1):
 					typeList.append(portType)
 					typeIndex = len(typeList) - 1
 
 			type = nativeType
-			if(type == None): type = portType + "_" + typeIndex
+			if(type == None): type = portType.__name__ + "_" + str(typeIndex)
 
 			types.append(type)
+		
+		return types
 
 	def extractType(store, ports):
 		for key, port in ports.items():
-			feature = port.portFeature
-
 			temp = store[key] = {}
 			i = 0
-			if(feature != None):
-				if(feature == portTypes.ArrayOf): i = 1
-				elif(feature == portTypes.Default): i = 2
-				elif(feature == portTypes.StructOf): i = 3
-				elif(feature == portTypes.Trigger): i = 4
-				elif(feature == portTypes.Union): i = 5
-				elif(feature == portTypes.VirtualType): i = 6
-				temp.feature = i
 
-			type
+			type = None
+			if(isinstance(port, dict)):
+				feature = port['feature']
+				if(feature != None):
+					if(feature == portTypes.ArrayOf): i = 1
+					elif(feature == portTypes.Default): i = 2
+					elif(feature == portTypes.StructOf): i = 3
+					elif(feature == portTypes.Trigger): i = 4
+					elif(feature == portTypes.Union): i = 5
+					elif(feature == portTypes.VirtualType): i = 6
+					temp['feature'] = i
+
 			if(i != 5):
-				type = typeToString([port.portType or port])[0]
-			else: type = typeToString(port.portType)
+				type = typeToString([
+					port['type'] if isinstance(port, dict) else port
+				])[0]
+			else: type = typeToString(port['type'])
 
-			temp.type = type
+			temp['type'] = type
 
 	def extract(obj, namespace):
 		for key, item in obj.items():
-			if(namespace == '' and key == 'BP'): continue
+			if(namespace == '' and key.startswith('BP')): continue
 
-			if(isinstance(item, Blackprint.Node)):
+			if(issubclass(item, Blackprint.Node)):
 				temp = list[namespace+key] = {}
 
 				if(item.input != None):
-					ref = temp.input = {}
+					ref = temp['input'] = {}
 					extractType(ref, item.input)
 				if(item.output != None):
-					ref = temp.output = {}
+					ref = temp['output'] = {}
 					extractType(ref, item.output)
 
-				temp.type = item.type
-				temp.interfaceSync = item.interfaceSync
+				temp['type'] = item.type
+				temp['interfaceSync'] = item.interfaceSync
 			elif(isinstance(item, dict)):
 				extract(item, namespace+key+"/")
 
 	extract(nodes, '')
-	if(options.raw): return list
+	if(('raw' in options) and options['raw']): return list
 
 	list = json.dumps(list)
-	return zlib.compress(list)
+	return zlib.compress(str.encode(list))
 
 @Blackprint.registerInterface("BPIC/BPRemote/PuppetNode")
 class _IPuppetNode(Blackprint.Interface):
