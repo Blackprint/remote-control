7f1cfb33770ec7e882cf85a34b5704ea5a80079e
StefansArya <stefansarya1@gmail.com>
2023-06-27 12:38:16 +0700

    Fix missing files and update to v0.1.11

--- js/utils/prepublish.js
+++ js/utils/prepublish.js
@@ -4,6 +4,15 @@ fs.mkdirSync('./dist');
 // Copy file to dist folder
 var temp = fs.readFileSync('../../dist/remote-control.min.js', 'utf8');
 fs.writeFileSync('./dist/remote-control.min.js', temp);
-
 var temp = fs.readFileSync('../../dist/remote-control.min.js.map', 'utf8');
 fs.writeFileSync('./dist/remote-control.min.js.map', temp);
+
+var temp = fs.readFileSync('../../dist/remote-control.sf.css', 'utf8');
+fs.writeFileSync('./dist/remote-control.sf.css', temp);
+var temp = fs.readFileSync('../../dist/remote-control.sf.css.map', 'utf8');
+fs.writeFileSync('./dist/remote-control.sf.css.map', temp);
+
+var temp = fs.readFileSync('../../dist/remote-control.sf.js', 'utf8');
+fs.writeFileSync('./dist/remote-control.sf.js', temp);
+var temp = fs.readFileSync('../../dist/remote-control.sf.js.map', 'utf8');
+fs.writeFileSync('./dist/remote-control.sf.js.map', temp);
